21e2adcb87558606700a4e5776398e1b37ae9ce3
Prakash Pandey <prakash.pandey@your-space.in>
2025-01-15 16:49:01 +0530

    sidebar setup

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,7 +1,20 @@
+import { AppSidebar } from "@/components/shared/app-sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div>
+      {" "}
+      <SidebarProvider>
+        <AppSidebar />
+        <main>
+          <SidebarTrigger />
+          {children}
+        </main>
+      </SidebarProvider>
+    </div>
+  );
 };
 
 export default layout;
